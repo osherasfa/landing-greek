ce3d28580be3490c09845d8813550ccc9b491e51
Osher <osherasfa@gmail.com>
2024-05-14 20:16:44 +0300

    1.0.0v

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="text-white bg-neutral-950 h-screen noise">
+      Hey
+    </div>
+  )
+}
+
+export default App
+
+// https://www.youtube.com/watch?v=Pz4Vslf55kg&ab_channel=SunnyBui
+// https://www.youtube.com/watch?v=Ci3J2Fk29gk&ab_channel=SonduckFilm
